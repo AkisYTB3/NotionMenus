cbe35bca83252ebf9970ebf3a818279debf7f0f9
Richard Mikloš <akisytbsteamy@gmail.com>
2025-04-14 22:37:08 +0200

    feat: remove notion command alias for notionmenus for compatibility with my main plugin Notion

--- src/main/java/org/notionsmp/notionMenus/commands/NotionMenusCommand.java
+++ src/main/java/org/notionsmp/notionMenus/commands/NotionMenusCommand.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.notionsmp.notionMenus.NotionMenus;
 
-@CommandAlias("notionmenus|nm|notion")
+@CommandAlias("notionmenus|nm")
 @CommandPermission("notionmenus.use")
 public class NotionMenusCommand extends BaseCommand {
 
